12e251ff32b1179196f1951c0a2d43c326557d28
Adrian <apaoletti@seidor.es>
2024-07-11 12:55:12 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,15 @@ import OneSignal from 'react-onesignal';
 
 function App() {
   useEffect(() => {
-    OneSignal.init({
-      appId: "20af5bf8-55ae-4425-a62e-55b749a4ca7f"
-    });
+    // window.OneSignalDeferred = window.OneSignalDeferred || [];
+    // window.OneSignalDeferred.push(async function(OneSignal) {
+    //   await OneSignal.init({
+    //     appId: "267a9b97-eabb-4f1e-8418-24960db42dd1",
+    //   });
+    // });
+  //   OneSignal.init({
+  //     appId: "20af5bf8-55ae-4425-a62e-55b749a4ca7f"
+  //   });
   }, []);
   return (
     <div className="App">
