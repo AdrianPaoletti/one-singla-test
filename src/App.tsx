4a0d1d78ef376c23a20ad3f1c6af4bb9bacd4c1d
Adrian <apaoletti@seidor.es>
2024-07-12 10:31:57 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,9 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <main>
-
-      </main>
+      <main></main>
     </div>
   );
 }
