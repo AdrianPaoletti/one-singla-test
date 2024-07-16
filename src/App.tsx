0b5f3ec3374353ca9cb8218e590324b000cee47d
Adrian <apaoletti@seidor.es>
2024-07-16 10:33:53 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
-import runOneSignal from "./oneSignal";
 // import { useEffect } from "react";
 // import OneSignal from "react-onesignal";
 
 function App() {
-  useEffect(() => {
-    runOneSignal()
+  // useEffect(() => {
+  //   runOneSignal()
 
-    return () => {};
-  }, []);
+  //   return () => {};
+  // }, []);
 
   return (
     <div className="App">
